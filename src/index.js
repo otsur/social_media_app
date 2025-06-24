c96d9a273d4dc5c87ff19e69d4cf40325ffdcd40
otsur <p.otsur@iitg.ac.in>
2025-06-24 18:56:37 +0530

    setup and start

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+
+
+connectDB()
+.then(() => {
+
+    app.on("error",(error) => {
+        console.log("ERROR:",error);
+        throw error
+    })
+
+
+    app.listen(process.env.PORT || 4000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.log("MONGODB connection failed",err);
+})
